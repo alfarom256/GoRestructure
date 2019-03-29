a20702329212e490e0ba53c8a741710e5b439af0
AlfaroM <alfaro.3@wright.edu>
2019-03-29 19:54:49 -0400

    0.4.0
    Update:
    Generated StructMangle functionality to generate random structs and append them to the files.
    
    Todo:
    StructMangle secret saws

--- GRLibAST/GRParseInline.go
+++ GRLibAST/GRParseInline.go
@@ -1,1 +1,22 @@
 package GRLibAST
+
+// this will serve as the implementation of a simple comment based markup language
+// basically it's gonna be like so
+
+/*
+
+some code
+some code
+some code
+some code
+some code
+//ObfuscateProtect:Start:string,int
+some code
+some code
+some code
+some code
+//ObfuscateProtect:Stop:string,int
+some code
+some code
+
+*/
